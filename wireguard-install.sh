9f402fcbd8d72e0759ed8a0a2ec4014653d0d0f0
Ev3r40 <74228472+rithsila@users.noreply.github.com>
2024-10-09 17:25:34 +0700

    Update wireguard-install.sh
    update

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Secure WireGuard server installer
-# https://github.com/angristan/wireguard-install
 
 RED='\033[0;31m'
 ORANGE='\033[0;33m'
@@ -103,7 +102,7 @@ function initialCheck() {
 
 function installQuestions() {
 	echo "Welcome to the WireGuard installer!"
-	echo "The git repository is available at: https://github.com/angristan/wireguard-install"
+	echo "The git repository is available at: rithsila.github.io"
 	echo ""
 	echo "I need to ask you a few questions before starting the setup."
 	echo "You can keep the default options and just press enter if you are ok with them."
